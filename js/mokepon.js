349eb1c369ae112cdd52fc2855490aeb0716ed70
Luis Fernando Farji <fernando.farji@gmail.com>
2022-08-13 10:12:35 -0300

    donde ubicar la etiqueta script

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,2 +1,5 @@
+function seleccionarMascotaJugador(){
+  alert('Seleccionaste tu mascota')
+}
 let botonMascotaJugador = document.getElementById('boton-mascota')
 botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
