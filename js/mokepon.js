4fe97392a7ee35fa5af6a47f6ffc0fedc94487cc
Luis Fernando Farji <fernando.farji@gmail.com>
2022-08-13 23:59:34 -0300

    Add eventlistener

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,5 +1,21 @@
-function seleccionarMascotaJugador(){
-  alert('Seleccionaste tu mascota')
-}
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+function iniciarJuego() {
+  let botonMascotaJugador = document.getElementById('boton-mascota')
+  botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+  }
+
+function seleccionarMascotaJugador() {
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    if (inputHipodoge.checked){
+      alert('Seleccionaste a Hipodoge')
+    } else if (inputCapipepo.checked){
+      alert('Seleccionaste a Capipepo')
+    } else if (inputRatigueya.checked){
+      alert('Seleccionaste a Ratigueya')
+    } else {
+      alert('Selecciona una mascota')
+    }
+  }
+
+window.addEventListener('load', iniciarJuego)
