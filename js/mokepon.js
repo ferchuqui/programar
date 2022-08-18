bb2928977096d18c35f3f2084ea753b424df9581
Luis Fernando Farji <fernando.farji@gmail.com>
2022-08-18 00:48:43 -0300

    Ganaste o perdiste

--- js/mokepon.js
+++ js/mokepon.js
@@ -64,18 +64,33 @@ function seleccionarMascotaJugador() {
     } else {
       ataqueEnemigo = 'TIERRA'
     }
-    crearMensaje()
+    combate()
+  }
+  function combate() {
+    if (ataqueEnemigo == ataqueJugador) {
+      crearMensaje("EMPATE");
+    } else if (ataqueJugador == "FUEGO" && ataqueEnemigo == "TIERRA") {
+      crearMensaje("GANASTE");
+    } else if (ataqueJugador == "AGUA" && ataqueEnemigo == "FUEGO") {
+      crearMensaje("GANASTE");
+    } else if (ataqueJugador == "TIERRA" && ataqueEnemigo == "AGUA") {
+      crearMensaje("GANASTE");
+    } else {
+      crearMensaje("PERDISTE");
+    }
   }
 
-  function crearMensaje() {
+  function crearMensaje(resultado) {
     let sectionMensajes = document.getElementById("mensajes");
     let parrafo = document.createElement("p");
     parrafo.innerHTML =
       "Tu mascota atacó con " +
       ataqueJugador +
       ", las mascota del enemigo atacó con " +
-      ataqueEnemigo
-    sectionMensajes.appendChild(parrafo)
+      ataqueEnemigo +
+      "- " +
+      resultado;
+    sectionMensajes.appendChild(parrafo);
   }
 
   function aleatorio(min, max){
