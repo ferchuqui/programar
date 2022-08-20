67c4acd8cb5c9f87abdcfe56cbc0b41cd3a2465f
Luis Fernando Farji <fernando.farji@gmail.com>
2022-08-20 11:07:33 -0300

    Reiniciando el juego

--- js/mokepon.js
+++ js/mokepon.js
@@ -11,6 +11,9 @@ function iniciarJuego() {
   botonAgua.addEventListener("click", ataqueAgua);
   let botonTierra = document.getElementById("boton-tierra");
   botonTierra.addEventListener("click", ataqueTierra);
+
+  let botonReiniciar = document.getElementById('boton-reiniciar')
+  botonReiniciar.addEventListener('click', reiniciarJuego)
 }
 function seleccionarMascotaJugador() {
   let inputHipodoge = document.getElementById("hipodoge");
@@ -112,6 +115,18 @@ function crearMensajeFinal(resultadoFinal) {
   let parrafo = document.createElement('p')
   parrafo.innerHTML =resultadoFinal
   sectionMensajes.appendChild(parrafo)
+
+  let botonFuego = document.getElementById("boton-fuego");
+  botonFuego.disabled = true
+  let botonAgua = document.getElementById("boton-agua");
+  botonAgua.disabled = true
+  let botonTierra = document.getElementById("boton-tierra");
+  botonTierra.disabled = true
+
+}
+
+function reiniciarJuego(){
+  location.reload();
 }
 function aleatorio(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
