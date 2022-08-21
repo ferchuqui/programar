ce220ce6f352cf9a6b52f50b46492b69b5b06af0
Luis Fernando Farji <fernando.farji@gmail.com>
2022-08-21 00:19:47 -0300

    ocultando elementos html

--- js/mokepon.js
+++ js/mokepon.js
@@ -6,6 +6,9 @@ function iniciarJuego() {
   let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
   sectionSeleccionarAtaque.style.display = 'none'
 
+  let sectionReiniciar = document.getElementById('reiniciar')
+  sectionReiniciar.style.display = 'none'
+
   let botonMascotaJugador = document.getElementById("boton-mascota");
   botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
   let botonFuego = document.getElementById("boton-fuego");
@@ -19,6 +22,11 @@ function iniciarJuego() {
   botonReiniciar.addEventListener('click', reiniciarJuego)
 }
 function seleccionarMascotaJugador() {
+  let sectionSeleccionarMascota = document.getElementById('seleccionar-mascota')
+  sectionSeleccionarMascota.style.display = 'none'
+  let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque");
+  sectionSeleccionarAtaque.style.display = "block";
+
   let inputHipodoge = document.getElementById("hipodoge");
   let inputCapipepo = document.getElementById("capipepo");
   let inputRatigueya = document.getElementById("ratigueya");
@@ -126,6 +134,8 @@ function crearMensajeFinal(resultadoFinal) {
   let botonTierra = document.getElementById("boton-tierra");
   botonTierra.disabled = true
 
+  let sectionReiniciar = document.getElementById('reiniciar')
+  sectionReiniciar.style.display = 'block'
 }
 
 function reiniciarJuego(){
